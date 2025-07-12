5ca2e72b5aa42d31550b025dbeaa09bc495ba882
Gabriel Moraes <moraesgabriel0003@gmail.com>
2025-07-12 14:02:56 -0300

    configurações iniciais da aplicação

--- CodigoFonte/back/gerenciadordetarefas/src/main/java/com/gabriel/gerenciadordetarefas/GerenciadordetarefasApplication.java
+++ CodigoFonte/back/gerenciadordetarefas/src/main/java/com/gabriel/gerenciadordetarefas/GerenciadordetarefasApplication.java
@@ -3,10 +3,18 @@ package com.gabriel.gerenciadordetarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class GerenciadordetarefasApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Define como variáveis de sistema
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(GerenciadordetarefasApplication.class, args);
 	}
 
