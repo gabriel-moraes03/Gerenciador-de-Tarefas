b7beb2c34107731f3fb292d84ffe90b47354edbb
Gabriel Moraes <moraesgabriel0003@gmail.com>
2025-07-12 00:06:46 -0300

    criando estrutura padrão back

---
+++ CodigoFonte/back/gerenciadordetarefas/src/main/java/com/gabriel/gerenciadordetarefas/GerenciadordetarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.gerenciadordetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordetarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordetarefasApplication.class, args);
+	}
+
+}
